e240b1ec7f66e1bb87f80459aba59c06003a0dc1
theregular <brkartch@gmail.com>
2024-12-01 15:53:43 -0700

    added DAO to part of StatusService

--- tweeter-server/src/model/service/StatusService.ts
+++ tweeter-server/src/model/service/StatusService.ts
@@ -5,8 +5,20 @@ import {
   Status,
   StatusDto,
 } from "tweeter-shared";
+import { IDAOFactory } from "../../daos/factory/IDAOFactory";
+import { IStatusDAO } from "../../daos/status/IStatusDAO";
+import { getDaoFactory } from "./getDaoFactory";
 
 export class StatusService {
+  private daoFactory: IDAOFactory;
+  private statusDAO: IStatusDAO;
+
+  constructor() {
+    this.daoFactory = getDaoFactory();
+    this.statusDAO = this.daoFactory.getStatusDAO();
+    // this.followDAO = this.daoFactory.getFollowDAO();
+  }
+
   async loadMoreStoryItems(
     authToken: AuthTokenDto,
     userAlias: string,
@@ -14,7 +26,13 @@ export class StatusService {
     lastItem: StatusDto | null
   ): Promise<[StatusDto[], boolean]> {
     // TODO: Replace with the result of calling server
-    return this.getFakeData(lastItem, pageSize);
+    // return this.getFakeData(lastItem, pageSize);
+    return this.statusDAO.getStoryPage(
+      authToken,
+      userAlias,
+      pageSize,
+      lastItem
+    );
   }
 
   async loadMoreFeedItems(
@@ -28,10 +46,11 @@ export class StatusService {
   }
 
   async postStatus(token: AuthTokenDto, newStatus: StatusDto): Promise<void> {
-    // Pause so we can see the logging out message. Remove when connected to the server
-    await new Promise((f) => setTimeout(f, 2000));
+    // await new Promise((f) => setTimeout(f, 2000));
 
     // TODO: Call the server to post the status
+
+    return this.statusDAO.postStatus(token, newStatus);
   }
 
   private async getFakeData(
